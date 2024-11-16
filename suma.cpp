226302bf804f83c6d8b64f5b5adf1724be06b1ef
ecuadros <ecuadros@spc.org.pe>
2024-11-15 19:44:00 -0500

    Resta implementada

--- suma.cpp
+++ suma.cpp
@@ -3,3 +3,7 @@
 int sumar(int a, int b) {
     return a + b;
 }
+
+int restar(int a, int b) {
+    return a - b;
+}
--- suma.h
+++ suma.h
@@ -2,5 +2,6 @@
 #define SUMA_H
 
 int sumar(int a, int b);
+int restar(int a, int b);
 
 #endif
