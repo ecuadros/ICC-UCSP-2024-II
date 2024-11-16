82a9eba884b76f11ea1448d3eef2f75e5f166571
Monte <montenegrojds@gmail.com>
2024-11-15 20:06:16 -0500

    comentarios

--- suma.cpp
+++ suma.cpp
@@ -1,17 +1,17 @@
 #include "suma.h"
-
+//esto es suma
 int sumar(int a, int b) {
     return a + b;
 }
-
+//esto es resta
 int restar(int a, int b) {
     return a - b;
 }
-
+//esto es una divicion
 int division(int a, int b){
     return a / b;
 }
-
+//esta es una multi
 int multiplicacion(int a, int b){
     return a * b;
 }
